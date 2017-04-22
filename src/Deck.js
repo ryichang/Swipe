fbe6d79fc00699ef77748047ebb23788a0a56930
ryichang <ryichang@gmail.com>
2017-04-21 20:37:34 -0700

    Added getCardStyle with transform

--- src/Deck.js
+++ src/Deck.js
@@ -17,20 +17,35 @@ class Deck extends Component {
     this.state = { panResponder, position };
   }
 
+  getCardStyle() {
+    return {
+      ...this.state.position.getLayout(),
+      transform: [{ rotate: '-45deg' }]
+    }
+  }
+
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map( (item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
       return this.props.renderCard(item)
     });
   }
 
   render() {
     return (
-      <Animated.View
-        style={this.state.position.getLayout()}
-        {...this.state.panResponder.panHandlers}
-      >
+      <View>
         {this.renderCards()}
-      </Animated.View>
+      </View>
     );
   }
 }
