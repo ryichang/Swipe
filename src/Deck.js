1d1a4039b5cdb2b50424807b7f59c7585821e07b
ryichang <ryichang@gmail.com>
2017-04-23 10:26:56 -0700

    Refactor Forceswipe for both directions

--- src/Deck.js
+++ src/Deck.js
@@ -17,9 +17,9 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          this.forceSwipeRight();
+          this.forceSwipe('right');
         } else if (gesture.dx < -SWIPE_THRESHOLD) {
-          console.log('swipe left!')
+          this.forceSwipe('left');
         } else {
           this.resetPosition();
         }
@@ -29,9 +29,10 @@ class Deck extends Component {
     this.state = { panResponder, position };
   }
 
-  forceSwipeRight() {
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
     Animated.timing(this.state.position, {
-      toValue: { x: SCREEN_WIDTH, y: 0 },
+      toValue: { x, y: 0 },
       duration: SWIPE_OUT_DURATION
     }).start();
   }
